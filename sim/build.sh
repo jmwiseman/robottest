1a8a9aa91eece299babbde79c3702d2ca232073a
phreaker2600 <swwiley@gmail.com>
2015-09-30 04:52:46 +0000

    actually simulates operator code interactivly

---
+++ sim/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#TODO:makefile
+
+cc -o isim ../src/opcontrol.c sim.c -I ./
+
--- sim/sim.c
+++ sim/sim.c
@@ -4,19 +4,26 @@
 #include <linux/joystick.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdbool.h>
+#define AXIS 2
+#define BUTTON 1
+extern void operatorControl(void);
 int sim_joyfd;
 int joy[11][2]; //larger then it neeeds to be
+int sim_motors[11];
 long sim_clock=0;
 void sim_delay(int ms) {
-	usleep(ms*100);
+	usleep(ms*10000);
+	sim_clock=sim_clock+ms;
 }
 void sim_joyinit() {
 	sim_joyfd=open("/dev/input/js1",O_RDONLY|O_NONBLOCK);
 	if(sim_joyfd)
 		printf("joystick open\n");
 }
-void sim_setmotor(int channel, int speed) {
-	printf("M\t%ld\t%d\t%d\n",sim_clock,channel,speed);
+void motorSet(int channel, int speed) {
+	if(channel<11)
+		sim_motors[channel]=speed;
 }
 void sim_getjoyevents() {
 	struct js_event e;
@@ -26,13 +33,42 @@ void sim_getjoyevents() {
 		}
 	}
 }
-int main () {
-	printf("Hello world\n");
-	sim_joyinit();
-	while(1) {
-		sim_getjoyevents();
-		sim_delay(20);
-		sim_clock=sim_clock+20;
+int joystickGetAnalog (unsigned char joystick, unsigned char axis) {
+	return joy[axis][AXIS];
+}
+bool joystickGetDigital (unsigned char joystick, unsigned char axis) {
+	return joy[axis][BUTTON]==0; 
+}
+void sim_ui()
+{
+	int i;
+	//printf("\033[;f");//home cursor
+	printf("\033[2J\n");
+	puts("\nMotors:");
+	for(i=0;i<11;i++) {
+		printf("%d\t",sim_motors[i]);
 	}
+	puts("\nJoystick");
+	for(i=0;i<5;i++){
+		printf("%d\t",joy[i][AXIS]);
+	}
+	puts("\nButtons");
+	for(i=0;i<11;i++){
+		printf("%d\t",joy[i][BUTTON]);
+	}
+}
+void sim_housekeeping()
+{
+	sim_ui();
+	sim_getjoyevents(); 
+}
+void delay(int t) {
+	sim_delay(t);
+	sim_housekeeping();
+}
+int main () {
+	printf("\033[2J\n");
+	sim_joyinit();
+	operatorControl();
 	return 0;
 }
