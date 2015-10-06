3b53fa99c5c1bc4d5a4f6bab5bfa3a6be4cfa7a0
Stephn Wile <swwiley@gmail.com>
2015-10-06 15:10:02 -0400

    automated tests should fully work for operator mode on small robot

--- sim/static.c
+++ sim/static.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sim.h>
+#include <math.h>
 #define SIMTIME 1000
+int axis;
+int value;
+int bad=0;
+int good=0;
+int iterations;
 //static tests
+int sign (int a) {
+	if(a==0)
+		return 0; //+1!=0!=-1
+	return abs(a)/a;
+}
+void die(char *a) {
+	printf("\n %s\nbad:%d\ngood:%d / %d\n",a,bad,good,iterations);
+	if(bad==0 && good==iterations)
+		printf("All PASSED!\n");
+	else
+		printf("some failures\n");
+	exit(0);
+}
 void sim_nextjoy() {
-	int axis=(sim_clock/20)%5;
-	int value=(((sim_clock/40)%3)*200)-200;
+	axis=(sim_clock/20)%5;
+	value=(((sim_clock/40)%3)*200)-200;
 	int i=0;
 	for(i=0;i<11;i++)
 		joy[i][AXIS]=0;
@@ -13,14 +32,53 @@ void sim_nextjoy() {
 	printf("time: %ld axis:%d value:%d\n",sim_clock,axis,value);
 }
 void status() {
+	int pass=0;
+	if(
+	(sim_motors[1] != sim_motors[2])||
+	(sim_motors[3] != sim_motors[4])) {
+		printf("Mortors are fighting against eachother!!");
+		bad=bad+1;
+		pass=-1;
+	}else {
+		if(axis==1) { //forward backward
+			if(
+			sign(sim_motors[1])==sign(sim_motors[3]) && 
+			sign(sim_motors[1])==sign(value)) {
+				pass=1;
+				good=good+1;
+			}else 
+				pass=-1;
+
+		}
+	
+		if(axis==3) { //left right
+			if(
+			sign(sim_motors[1])!=sign(sim_motors[3]) && 
+			sign(sim_motors[1])==sign(value)) {
+				pass=1;
+				good=good+1;
+			}else 
+				pass=-1;
+			
+		}
+	}//motor fight else
+	printf("Good: %d Bad: %d Drive Motors: %d %d %d %d \n ",good,bad,
+	sim_motors[1],
+	sim_motors[2],
+	sim_motors[3],
+	sim_motors[4]);
+	if(pass>0)
+		printf("\tPass\n");
+	else
+		printf("\tFail\n");
 }
 void sim_housekeeping() {
+	iterations+=1;
+	status();
 	sim_nextjoy();
 	if(sim_clock>SIMTIME){
-		printf("done\n");
-		exit(0);
+		die("done");
 	}
-	status();
 }
 int main() {
 	printf("testing...\n");
