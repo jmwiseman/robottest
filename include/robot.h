ce2decd8074d701184059ac9628ac9023e1ad87b
jmwiseman <jmwiseman@liberty.edu>
2015-10-17 14:26:20 -0400

    forgot this

---
+++ include/robot.h
@@ -0,0 +1,17 @@
+#define JOY_TURN 1
+#define JOY_FORWARD 2
+#define JOY_CONVEYER 3
+#define JOY_FLYWHEEL 6
+#define MO_CONVEYER1 1
+#define MO_CONVEYER2 10
+//#define MO_CONVEYER1 4
+//#define MO_CONVEYER2 5
+#define MO_LEFT1 1
+#define MO_LEFT2 2
+#define MO_RIGHT1 10
+#define MO_RIGHT2 4
+
+#define FLY_SPEED 100
+#define MO_FLY1 6
+#define MO_FLY2 7
+
