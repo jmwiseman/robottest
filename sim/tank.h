04c2ca254593b8bbde801594da39ddabf1edaeb8
phreaker2600 <swwiley@gmail.com>
2015-10-29 23:27:33 +0000

    oops agian

---
+++ sim/tank.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define PI 3.14159
+extern void autonomous(void);
+FILE *fp;
+typedef struct tank {
+	double x,y,h;
+	double lspeed,rspeed;
+	double width;//not necesary?
+	double battery;
+} tank;
+tank timothy;
+void sim_setsensors(tank *v);
+
