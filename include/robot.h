aa7fa3c0eb75c350f9b7581de3e851e3237a46d6
jmwiseman <jmwiseman@liberty.edu>
2015-10-29 20:33:05 -0400

    Added an intake control untested

--- include/robot.h
+++ include/robot.h
@@ -1,6 +1,6 @@
 #define JOY_TURN 1
 #define JOY_FORWARD 2
-#define JOY_CONVEYER 3
+#define JOY_INTAKE 3
 #define JOY_FLYWHEEL 6
 #define JOY_DEAD 20
 #define JOY_SCALE 2
@@ -11,17 +11,19 @@
 #define MO_CONVEYER2 9
 #define B_CONVEYER 5
 #define CONVEYER_SPEED 150
-#define CONVEYER_SPEED2 200
+
+#define MO_INTAKE 10
+#define INTAKE_SPEED 150
 
 #define MO_LEFT1 2
 #define MO_LEFT2 4
 #define MO_RIGHT1 3
 #define MO_RIGHT2 5
 
-#define FLY_SPEED 255
-
 #define MO_FLY1 6
 #define MO_FLY2 7
+#define FLY_SPEED 255
+
 #define LS_LEFT 1
 #define LS_CENTER 2
 #define LS_RIGHT 3
