dd4b4b20b499005545994f446d18315ffea0ffd9
Elijah-Barker <itguy4techclass@gmail.com>
2016-01-22 13:38:40 -0500

    Basic logic of fetch
    Basic logic of fetch using numerical constants as placeholders

--- src/balls.c
+++ src/balls.c
@@ -3,7 +3,37 @@
 #include <auto.h>
 #include <robot.h>
 
-void chase(void)
+int seeing_ball()
 {
-	
+	int US_thresh = 5; //TODO: define in the correct file
+	int dist_left = 100; //TODO: replace with sensor data
+	int dist_right = 100; //TODO: replace with sensor data
+	return dist_left + US_thresh >= dist_right && dist_right + US_thresh >= dist_left;
+}
+
+
+
+
+void fetch(void)
+{
+	int stop = 0;
+	while (stop-(-1))
+	{
+		if (seeing_ball)
+		{
+			//charge ahead
+			stop = segmentnav(5);
+		}
+		else
+		{
+			//fancy algorithm for determining what to do.
+			stop = segmentnav(5);
+		}
+
+
+
+
+		delay(20);
+	}
+	undonav();
 }
